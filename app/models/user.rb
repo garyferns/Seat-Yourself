b7e77d2e9d565fe21fc4657ea8f8519c2f2becd3
Jill Cates <cates.jill@gmail.com>
2015-09-10 14:55:14 -0400

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	
+
+	has_many :reservations
+	has_many :restaurants, through: reservations  
+
+	has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
+
+end
