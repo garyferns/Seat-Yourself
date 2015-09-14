118f891cfc36b7502c122a903ced7a5e0c22d6e0
Jill Cates <cates.jill@gmail.com>
2015-09-14 16:01:27 -0400

    Capacity validation implemented

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,21 @@
 class Reservation < ActiveRecord::Base
   
-  RESO_TIMES = ['18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30']
-
   belongs_to :user
   belongs_to :restaurant
 
   validates :party_size, presence: true 
+  validate :capacity_exists
   
+ def capacity_exists
+  	existing_reservations = Reservation.where("restaurant_id = ? and dinner_time > ? and dinner_time < ?")
+  	total_people = existing_reservations.sum(:party_size)
+  	restaurant_capacity = self.restaurant.capacity
+  	if (self.party_size + total_people) > restaurant_capacity
+		# errors.add(:party_size, "Capacity exceeded")
+		errors[:base] << "Capacity exceeded"
+	end  
+end 
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
 	has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
 
+	validates :first_name, presence: true
+	validates :last_name, presence: true
 end
